2f0b75ce2ad7eb07793a8ec4a382f9f0b8fc6dbf
Kris Wallsmith <kris.wallsmith@gmail.com>
2011-01-19 07:03:21 -0800

    inflector

--- src/Symfony/Component/DependencyInjection/Factory/StaticFactory.php
+++ src/Symfony/Component/DependencyInjection/Factory/StaticFactory.php
@@ -45,6 +45,11 @@ class StaticFactory implements FactoryInterface
         return static::$serviceIds;
     }
 
+    static public function camelize($id)
+    {
+        return preg_replace(array('/(?:^|_)+(.)/e', '/\.(.)/e'), array("strtoupper('\\1')", "'_'.strtoupper('\\1')"), $id);
+    }
+
     static public function underscore($id)
     {
         return strtolower(preg_replace(array('/([A-Z]+)([A-Z][a-z])/', '/([a-z\d])([A-Z])/'), array('\\1_\\2', '\\1_\\2'), strtr($id, '_', '.')));
