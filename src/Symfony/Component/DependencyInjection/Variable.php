6f1ff9781e258b56339f022435dc7e81a4f6c69c
Kris Wallsmith <kris.wallsmith@gmail.com>
2011-01-19 04:47:15 -0800

    builder classes

---
+++ src/Symfony/Component/DependencyInjection/Variable.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien.potencier@symfony-project.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\DependencyInjection;
+
+class Variable
+{
+    protected $name;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function __toString()
+    {
+        return $this->name;
+    }
+}
